8676ae7e7ddcd30e78dc314c719d592418179dad
Aadhitya A <59508546+echo-864@users.noreply.github.com>
2020-08-29 21:59:54 +0530

    Create footer and add color

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#440047`,
       marginBottom: `1.45rem`,
     }}
   >
